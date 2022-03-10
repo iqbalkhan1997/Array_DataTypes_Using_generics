96273444f8f75343f985835416aba092a4f7f15a
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-10 23:14:21 +0530

    Added Generics functionality to print all datatypes using array

--- src/com/blz/generics_array/ArrayGenerics.java
+++ src/com/blz/generics_array/ArrayGenerics.java
@@ -1,47 +1,19 @@
 package com.blz.generics_array;
 
 public class ArrayGenerics {
-    Integer intArray[];
-    Double doubleArray[];
-    Character charArray[];
-
-    ArrayGenerics(Integer[] intArray){
-        this.intArray=intArray;
+    private static <E> void toPrint(E[] Array) {
+        for (E value : Array) {
+            System.out.print(value + ",");
+        }
     }
-    ArrayGenerics(Character[] charArray) {
-        this.charArray = charArray;
-    }
-    ArrayGenerics(Double[] doubleArray){
-        this.doubleArray=doubleArray;
-    }
-
     public static void main(String[] args) {
-        Integer[] intArray ={22,54,48,96,};
-        Double[] doubleArray ={2.1,5.8,7.3,5.5};
-        Character[] charArray ={'A', 'B', 'C', 'D', 'S'};
-        ArrayGenerics printIntArray = new ArrayGenerics(intArray);
-        printIntArray.toPrint(printIntArray.intArray);
+        Integer[] intArray = {22, 54, 48, 96,};
+        Double[] doubleArray = {2.1, 5.8, 7.3, 5.5};
+        Character[] charArray = {'A', 'B', 'C', 'D', 'S'};
+        ArrayGenerics.toPrint(intArray);
         System.out.println();
-        ArrayGenerics printDoubleArray = new ArrayGenerics(doubleArray);
-        printDoubleArray.toPrint(printDoubleArray.doubleArray);
+        ArrayGenerics.toPrint(doubleArray);
         System.out.println();
-        ArrayGenerics printCharArray = new ArrayGenerics(charArray);
-        printCharArray.toPrint(printCharArray.charArray);
-
-    }
-    private void toPrint(Integer[] Array){
-        for(Integer value : Array){
-            System.out.print(value+",");
-        }
-    }
-    private void toPrint(Double[] Array){
-        for(Double value : Array){
-            System.out.print(value+",");
-        }
-    }
-    private void toPrint(Character[] Array){
-        for(Character value : Array){
-            System.out.print(value+",");
-        }
+        ArrayGenerics.toPrint(charArray);
     }
 }
