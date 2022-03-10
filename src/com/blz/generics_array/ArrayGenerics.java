c82f89b7fbaef308b6d45f055ea8d77b4c35346b
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-10 23:00:04 +0530

    Added: welcome message

---
+++ src/com/blz/generics_array/ArrayGenerics.java
@@ -0,0 +1,7 @@
+package com.blz.generics_array;
+
+public class ArrayGenerics {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to generics example program using array with all data types");    }
+}
