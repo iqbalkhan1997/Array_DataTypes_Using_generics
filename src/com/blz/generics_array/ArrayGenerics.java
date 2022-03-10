7978ccd9f3c77f5facbcdf4f75c62033d7a2a943
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-03-10 23:02:44 +0530

    Added:printing all data types with array

--- src/com/blz/generics_array/ArrayGenerics.java
+++ src/com/blz/generics_array/ArrayGenerics.java
@@ -1,7 +1,47 @@
 package com.blz.generics_array;
 
 public class ArrayGenerics {
+    Integer intArray[];
+    Double doubleArray[];
+    Character charArray[];
+
+    ArrayGenerics(Integer[] intArray){
+        this.intArray=intArray;
+    }
+    ArrayGenerics(Character[] charArray) {
+        this.charArray = charArray;
+    }
+    ArrayGenerics(Double[] doubleArray){
+        this.doubleArray=doubleArray;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to generics example program using array with all data types");    }
+        Integer[] intArray ={22,54,48,96,};
+        Double[] doubleArray ={2.1,5.8,7.3,5.5};
+        Character[] charArray ={'A', 'B', 'C', 'D', 'S'};
+        ArrayGenerics printIntArray = new ArrayGenerics(intArray);
+        printIntArray.toPrint(printIntArray.intArray);
+        System.out.println();
+        ArrayGenerics printDoubleArray = new ArrayGenerics(doubleArray);
+        printDoubleArray.toPrint(printDoubleArray.doubleArray);
+        System.out.println();
+        ArrayGenerics printCharArray = new ArrayGenerics(charArray);
+        printCharArray.toPrint(printCharArray.charArray);
+
+    }
+    private void toPrint(Integer[] Array){
+        for(Integer value : Array){
+            System.out.print(value+",");
+        }
+    }
+    private void toPrint(Double[] Array){
+        for(Double value : Array){
+            System.out.print(value+",");
+        }
+    }
+    private void toPrint(Character[] Array){
+        for(Character value : Array){
+            System.out.print(value+",");
+        }
+    }
 }
